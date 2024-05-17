deedce06d17b30468154441a92a298f6b0661a84
OmarThinks <24939461+OmarThinks@users.noreply.github.com>
2024-05-18 02:28:53 +0300

    redux: installed

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -10,7 +10,6 @@ import { useEffect } from "react";
 import "react-native-reanimated";
 
 import { useColorScheme } from "@/hooks/useColorScheme";
-import { SafeAreaView } from "react-native";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
