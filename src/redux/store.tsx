dadfd7eb80b50dfd8b7f4715a9229abb5ba86cb9
OmarThinks <24939461+OmarThinks@users.noreply.github.com>
2024-05-18 02:45:21 +0300

    redux: progress

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,5 +1,20 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { useDispatch, useSelector } from "react-redux";
+import counterReducer from "./features/counter/counterSlice";
 
-export default configureStore({
-  reducer: {},
+const store = configureStore({
+  reducer: {
+    counter: counterReducer,
+  },
 });
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+type RootState = ReturnType<typeof store.getState>;
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+type AppDispatch = typeof store.dispatch;
+
+const useAppDispatch = useDispatch.withTypes<AppDispatch>();
+const useAppSelector = useSelector.withTypes<RootState>();
+
+export default store;
+export type { AppDispatch, RootState, useAppDispatch, useAppSelector };
