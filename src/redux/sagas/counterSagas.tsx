259318ea9b948cb1f349934d98d6a85cd5b0676f
OmarThinks <24939461+OmarThinks@users.noreply.github.com>
2024-05-18 07:24:14 +0300

    saga: up and running

--- src/redux/sagas/counterSagas.tsx
+++ src/redux/sagas/counterSagas.tsx
@@ -1,17 +1,17 @@
 import { put, takeEvery } from "redux-saga/effects";
-import { increment } from "../features/counter/counterSlice";
+import { increment, incrementAsync } from "../features/counter/counterSlice";
 
 const delay = (ms: number) => new Promise((res) => setTimeout(res, ms));
 
 // Our worker Saga: will perform the async increment task
-function* incrementAsync() {
+function* incrementAsyncSaga() {
   yield delay(1000);
   yield put(increment());
 }
 
 // Our watcher Saga: spawn a new incrementAsync task on each INCREMENT_ASYNC
 function* watchIncrementAsync() {
-  yield takeEvery(incrementAsync, incrementAsync);
+  yield takeEvery(incrementAsync.type, incrementAsyncSaga);
 }
 
 export { watchIncrementAsync };
