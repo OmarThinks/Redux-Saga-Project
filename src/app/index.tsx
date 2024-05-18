96a58a7755c48646e2e1f26a7d3dd29358f3d99d
OmarThinks <24939461+OmarThinks@users.noreply.github.com>
2024-05-18 04:19:51 +0300

    Home: progress

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,5 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, Button, Pressable } from "react-native";
+import React, { memo } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useCounter } from "@/redux/store";
 
@@ -8,9 +8,34 @@ export default function index() {
 
   return (
     <SafeAreaView style={{}}>
-      <View className="h-full self-stretch items-stretch bg-slate-300 ">
-        <Text>{counter}</Text>
+      <View
+        className="h-full self-stretch items-stretch justify-center"
+        style={{ gap: 20 }}
+      >
+        <Text className="text-[30px] text-center">{counter}</Text>
+
+        <View className="flex-row self-stretch justify-center flex-wrap">
+          <ActionButton text={"-"} onPress={() => {}} />
+          <ActionButton text={"+"} onPress={() => {}} />
+          <ActionButton text={"+"} onPress={() => {}} />
+        </View>
       </View>
     </SafeAreaView>
   );
 }
+
+const ActionButton = memo(
+  ({ text, onPress }: { text: string; onPress: () => void }) => {
+    return (
+      <View className="rounded-[10px] overflow-hidden border-[1px]">
+        <Pressable
+          className=" w-[50px] h-[50px] items-center justify-center"
+          onPress={onPress}
+          android_ripple={{ color: "grey", borderless: false }}
+        >
+          <Text className="text-[20px]">{text}</Text>
+        </Pressable>
+      </View>
+    );
+  }
+);
